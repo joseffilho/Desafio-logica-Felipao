e8dd2128357a2c523d6b0d49c6f36ec1437c3597
joseffilho <filhoserginho63@gmail.com>
2023-12-17 19:54:25 -0300

    Primeiro desafio do Felipao

---
+++ index.js
@@ -0,0 +1,23 @@
+let nome = "Sérgio"
+let xp = 11000
+if (xp < 1000){
+    console.log("O Herói de nome " + nome + " está no nível de FERRO")
+} else if(xp >= 1001 && xp <= 2000){
+    console.log("O Herói de nome " + nome + " está no nível de BRONZE")
+} else if(xp >= 2001 && xp <= 5000){
+    console.log("O Herói de nome " + nome + " está no nível de PRATA")
+} else if(xp >= 5001 && xp <= 7000){
+    console.log("O Herói de nome " + nome + " está no nível de OURO")
+} else if(xp >= 7001 && xp <= 8000){
+    console.log("O Herói de nome " + nome + " está no nível de PLATINA")
+} else if(xp >= 8001 && xp <= 9000){
+    console.log("O Herói de nome " + nome + " está no nível de ASCENDENTE")
+} else if(xp >= 9001 && xp <= 10000){
+    console.log("O Herói de nome " + nome + " está no nível de IMORTAL")
+} else {
+    console.log("O Herói de nome " + nome + " está no nível de RADIANTE")
+}
+
+
+
+
